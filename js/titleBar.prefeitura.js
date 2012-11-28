9fccfbc845eb36b0f04c372a9d923e9d91b6e4a0
Henry Shinohara <shin@aware.com.br>
2012-11-28 00:33:36 -0200

    Alterando textos dos titulos do admin da prefeitura

---
+++ js/titleBar.prefeitura.js
@@ -0,0 +1,40 @@
+var titleBarContent = {
+					"login":{
+								"title": "Login",
+								"tip": "Entre com seu usuário e senha:"
+								},
+					"dashboard":{
+								"title": "Dashboard",
+								"tip": "Bem vindo &agrave; interface administrativa de relat&oacute;rios do Programa Cidade Sustent&aacute;veis!<br />Um dos objetivos do Programa Cidades Sustent&aacute;veis &eacute; fortalecer o trabalho dos Munic&iacute;pios nas &aacute;reas da gest&atilde;o, planejamento, transpar&ecirc;ncia, participa&ccedil;&atilde;o, presta&ccedil;&atilde;o de contas, entre outros. Sempre incorporando a Sustentabilidade de forma transversal &agrave;s pol&iacute;ticas p&uacute;blicas.<br />Nesse sentido, indicadores e metas s&atilde;o ferramentas fundamentais de trabalho.<br />A orienta&ccedil;&atilde;o que damos &eacute; tentar elaborar e disponibilizar o m&aacute;ximo de indicadores sugeridos pelo Programa Cidades Sustent&aacute;veis. Para isto &eacute; importante trabalhar de forma coordenada junto aos diferentes &oacute;rg&atilde;os e n&iacute;veis da administra&ccedil;&atilde;o p&uacute;blica, municipal, estadual e federal que possam fornecer dados, informa&ccedil;&otilde;es e indicadores de todas as &aacute;reas previstas no Programa.<br />Ficamos a disposi&ccedil;&atilde;o para qualquer outro esclarecimento.<br />Orienta&ccedil;&otilde;es b&aacute;sicas:<br />No seu primeiro acesso voc&ecirc; deve mudar a sua senha no item &quot;Prefer&ecirc;ncias&quot; e preencher os dados do munic&iacute;pio. &Eacute; muito importante cadastrar uma pessoa respons&aacute;vel pelas informa&ccedil;&otilde;es inseridas no sistema.<br />No item &quot;Vari&aacute;veis b&aacute;sicas&quot; voc&ecirc; far&aacute; o cadastro de dados b&aacute;sicos do seu munic&iacute;pio, parte dessas informa&ccedil;&otilde;es ser&atilde;o usadas para compor a p&aacute;gina principal do seu munic&iacute;pio.<br />No item &quot;Indicadores&quot; voc&ecirc; far&aacute; a navega&ccedil;&atilde;o pelos eixos do Programa Cidades Sustent&aacute;veis e preencher os dados para compor os 100 indicadores b&aacute;sicos do Programa Cidades Sustent&aacute;veis.<br />Cada item tem a explica&ccedil;&atilde;o de como usar o sistema.<br />Obrigado.<br />Programa Cidades Sustent&aacute;veis."
+								},
+					"users":{
+							"title": "Usuários",
+							"tip": "Veja os usuários já cadastrados. Utilize o botão 'Adicionar' para criar um novo usuário."},
+					"users,add":{
+							"title": "Novo Usuário",
+							"tip": "Preencha os campos abaixo para adicionar um novo Usuário."},
+					"cities":{
+							"title": "Cidades",
+							"tip": ""},
+					"variable":{
+							"title": "Variáveis",
+							"tip": "Veja as variáveis já cadastradas. Utilize o botão 'Adicionar' para criar uma nova variável."},
+					"myvariable":{
+							"title": "Variáveis Básicas",
+							"tip": "As vari&aacute;veis b&aacute;sicas s&atilde;o usadas na p&aacute;gina principal de apresenta&ccedil;&atilde;o do munic&iacute;pio e na composi&ccedil;&atilde;o de indicadores. Para adicionar um novo valor, clique na figura do l&aacute;pis para edi&ccedil;&atilde;o da vari&aacute;vel.<br />Ser&aacute; apresentado o nome da vari&aacute;vel, um campo em branco para voc&ecirc; preencher o valor, a periodicidade que voc&ecirc; ter&aacute; que preencher a vari&aacute;vel, a data que voc&ecirc; escolher&aacute; para aquele dado e uma descri&ccedil;&atilde;o do que &eacute; a vari&aacute;vel.<br />Ap&oacute;s preencher o valor da vari&aacute;vel e o ano a que ela corresponde, clique em &quot;Adicionar&quot;.<br />O primeiro ano que voc&ecirc; deve preencher &eacute; o ano anterior ao in&iacute;cio de sua gest&atilde;o, ou seja, 2012. Ap&oacute;s os dados serem preenchidos, ser&aacute; apresentado um quadro de s&eacute;rie hist&oacute;rica, portanto se voc&ecirc; possui dados anteriores a 2012, &eacute; importante que sejam preenchidos no sistema."},
+					"indicator":{
+							"title": "Indicadores",
+							"tip": "Veja os indicadores já cadastrados. Utilize o botão 'Adicionar' para criar um novo indicador."},
+					"myindicator":{
+							"title": "Indicadores",
+							"tip": "Os indicadores aparecer&amp;atilde;o com 3 cores diferentes, referentes a: nenhum dado preenchido (rosa), preenchido mas n&amp;atilde;o com o ano anterior ao vigente (azul) e preenchido (branco). Conforme a legenda.<br />Os indicadores s&amp;atilde;o apresentados pelos eixos do Programa Cidades Sustent&amp;aacute;veis. Para preencher os dados voc&amp;ecirc; deve clicar no eixo, escolher o indicador que ser&amp;aacute; preenchido e clicar no l&amp;aacute;pis para a edi&amp;ccedil;&amp;atilde;o.<br />Aparecer&amp;aacute; na tela o c&amp;aacute;lculo (f&amp;oacute;rmula) que ser&amp;aacute; feito com os dados que forem preenchidos, ou seja, o c&amp;aacute;lculo que ser&amp;aacute; feito para compor aquele indicador e a periodicidade que voc&amp;ecirc; dever&amp;aacute; atualiza-lo.<br />Escolha o ano a que o dado se refere (exemplo: 2012) e clique em cadastrar.<br />No cadastro voc&amp;ecirc; preencher&amp;aacute; os dados necess&amp;aacute;rios para compor o indicador: valor da vari&amp;aacute;vel, a fonte de onde vem o dado (Exemplo: Secretaria Municipal de Sa&amp;uacute;de), observa&amp;ccedil;&amp;otilde;es t&amp;eacute;cnicas sobre o dado e uma meta estabelecida pelo seu munic&amp;iacute;pio para o indicador.<br />Ap&amp;oacute;s preencher os dados, clique em enviar.<br />Essas informa&amp;ccedil;&amp;otilde;es aparecer&amp;atilde;o e na p&amp;aacute;gina dos indicadores do seu munic&amp;iacute;pio. Se voc&amp;ecirc; por algum motivo n&amp;atilde;o tem os dados necess&amp;aacute;rios, clique em &amp;quot;N&amp;atilde;o possuo os dados&amp;quot;.<br />Ap&amp;oacute;s o cadastro aparecer&amp;aacute; para voc&amp;ecirc; uma tabela com os dados inseridos no sistema e o valor calculado do indicador. Voc&amp;ecirc; sempre preencher&amp;aacute; dados do ano anterior ao vigente, portanto Voc&amp;ecirc; n&amp;atilde;o poder&amp;aacute; editar os dados. Caso necessite fazer a edi&amp;ccedil;&amp;atilde;o, preencha o formul&amp;aacute;rio de fale conosco solicitando."},
+					"tokens":{
+							"title": "Tokens",
+							"tip": ""},
+					"reports":{
+							"title": "Relatórios",
+							"tip": ""},
+					"prefs":{
+							"title": "Preferências",
+							"tip": "Nas prefer&ecirc;ncias, voc&ecirc; deve preencher os dados da prefeitura, o endere&ccedil;o, telefone, email etc. N&atilde;o se esquece de mudar a senha inicial do sistema e preencher o nome da pessoa que ser&aacute; respons&aacute;vel pelo cadastro/inclus&atilde;o de dados. Voc&ecirc; deve anexar o arquivo em pdf do seu Programa de Metas."},
+				  };

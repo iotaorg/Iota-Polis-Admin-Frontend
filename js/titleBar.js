3ae74527454e1a467df0279181d2243d42eac9be
Henry Shinohara <shin@aware.com.br>
2013-02-26 15:39:57 -0300

    Update nos Textos das telas

--- js/titleBar.js
+++ js/titleBar.js
@@ -5,7 +5,7 @@ var titleBarContent = {
 								},
 					"dashboard":{
 								"title": "Interface administrativa",
-								"tip": "Bem vindo à interface administrativa de relatórios do  Programa Cidades Sustentáveis!<br><br>Um dos objetivos do Programa Cidades Sustentáveis é  fortalecer o trabalho dos Municípios nas áreas da gestão, planejamento,  transparência, participação, prestação de contas, entre outros. Tudo isso sempre  incorporando a Sustentabilidade de forma transversal às políticas públicas.<br><br>E, neste sentido, indicadores e metas são ferramentas fundamentais  de trabalho.<br><br>Nossa orientação é que seja elaborado e disponibilizado o  máximo de indicadores sugeridos pelo Programa Cidades Sustentáveis. Para isto,  é importante trabalhar de forma coordenada junto aos diferentes órgãos e níveis  da administração pública (municipal, estadual e federal) que possam fornecer  dados, informações e indicadores de todas as áreas previstas no Programa.<br><br>Estamos à disposição para qualquer outro esclarecimento.<br><br>Orientações básicas:<br><br>No primeiro acesso é preciso mudar a senha no item  &quot;Preferências&quot; e preencher os dados do município. É muito importante  cadastrar uma pessoa responsável pelas informações que serão inseridas no  sistema.<br><br>No item &quot;Variáveis básicas&quot; você fará o cadastro  de dados básicos do seu município. Parte destas informações será usada para  compor a página principal de cada cidade.<br><br>No item &quot;Indicadores&quot; você fará a navegação pelos  eixos do Programa Cidades Sustentáveis e preencherá os dados para compor os 100  indicadores básicos do Programa Cidades Sustentáveis.<br><br>Obrigado.<br><br>Programa  Cidades Sustentáveis."
+								"tip": ""
 								},
 					"users":{
 							"title": "Usuários",
@@ -21,16 +21,22 @@ var titleBarContent = {
 							"tip": "Veja as variáveis já cadastradas. Utilize o botão 'Adicionar' para criar uma nova variável."},
 					"myvariable":{
 							"title": "Variáveis Básicas",
-							"tip": "As variáveis básicas são usadas na página principal de  apresentação do município e na composição de indicadores. Para adicionar um  novo valor, clique na figura do lápis.<br><br>A partir daí será apresentado o nome da variável, um campo  em branco para você informar o valor, a periodicidade com a qual você terá que  preencher a variável, a data que você escolherá para aquele dado e uma  descrição do que é a variável.<br><br>Após preencher o valor da variável e o ano a que ela  corresponde, clique em &quot;Adicionar&quot;.<br><br>O primeiro ano que você deve informar é o anterior ao início  de sua gestão, ou seja, 2012. Após os dados serem preenchidos, será apresentado  um quadro de série histórica. Assim, se há dados anteriores a 2012, é  importante que sejam inseridos no sistema."},
+							"tip": ""},
+					"myvariableedit":{
+							"title": "Editar Valores",
+							"tip": ""},
 					"indicator":{
 							"title": "Indicadores",
 							"tip": "Veja os indicadores já cadastrados. Utilize o botão 'Adicionar' para criar um novo indicador."},
 					"myindicator":{
 							"title": "Indicadores",
-							"tip": "Os indicadores aparecerão com três cores diferentes, nas  seguintes classificações: nenhum dado preenchido (vermelho); preenchido, mas  não com o ano anterior ao vigente (laranja); e preenchido (azul).<br><br>Os indicadores são apresentados pelos eixos do Programa  Cidades Sustentáveis. Para preencher os dados você deve clicar no eixo,  escolher o indicador que será preenchido e clicar no lápis para a edição."},
+							"tip": ""},
 					"myindicator,edit":{
 							"title": "Indicadores",
-							"tip": "Aparecerá na tela o cálculo (fórmula) que será feito com os dados que forem preenchidos, ou seja, o cálculo que será feito para compor aquele indicador e a periodicidade que você deverá atualiza-lo.<br /><br />Escolha o ano a que o dado se refere (exemplo: 2012) e clique em cadastrar.<br /><br />No cadastro você preencherá os dados necessários para compor o indicador: valor da variável, a fonte de onde vem o dado (Exemplo: Secretaria Municipal de Saúde), observações técnicas sobre o dado e uma meta estabelecida pelo seu município para o indicador.<br /><br />Após preencher os dados, clique em enviar.<br /><br />Essas informações aparecerão e na página dos indicadores do seu município. Se você por algum motivo não tem os dados necessários, clique em &quot;Não possuo os dados&quot;.<br /><br />Após o cadastro aparecerá para você uma tabela com os dados inseridos no sistema e o valor calculado do indicador. Você sempre preencherá dados do ano anterior ao vigente, portanto Você não poderá editar os dados. Caso necessite fazer a edição, preencha o formulário de fale conosco solicitando."},
+							"tip": ""},
+					"mygroup":{
+							"title": "Grupo de Indicadores",
+							"tip": ""},
 					"tokens":{
 							"title": "Tokens",
 							"tip": ""},
@@ -39,5 +45,5 @@ var titleBarContent = {
 							"tip": ""},
 					"prefs":{
 							"title": "Preferências",
-							"tip": "Nas preferências, você deve preencher os dados básicos da  prefeitura – endereço, telefone, email etc. Não se esqueça de mudar a senha  inicial do sistema e preencher o nome da pessoa que será responsável pelo  cadastro/inclusão de dados. Você deve anexar o arquivo em pdf com seu Programa  de Metas."},
+							"tip": ""},
 				  };

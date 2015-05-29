c47fa96a4e480e22c013520fb9a723fc84c42602
RenatoCRON <renato.cron@gmail.com>
2015-05-29 08:57:06 -0300

    ja print o add

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -2,6 +2,6 @@
 
 export GIT_DIR=$(git rev-parse --show-toplevel)
 
-(sleep 2; docker inspect --format '{{ .NetworkSettings.IPAddress }}' iota-frontend ) &
+(sleep 2; export X=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' iota-frontend 2>&1`; echo "http://$X/frontend" ) &
 docker run --rm  --name iota-frontend  -v $GIT_DIR:/data/frontend:ro iota/frontend-nginx
 

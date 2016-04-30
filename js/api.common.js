8b96e5959479876ed22de44566fb715b29f4b85b
RenatoCRON <renato.cron@gmail.com>
2016-04-30 15:59:01 -0300

    fix flash screen when opening

--- js/api.common.js
+++ js/api.common.js
@@ -56,7 +56,7 @@ if (!String.prototype.render) {
 					if (typeof v == 'string') {
 						n = 0;
 					}
- 
+
 					if (n == 0 && (
 						k.match(/password/) ||
 						k.match(/email/) ||
@@ -491,12 +491,17 @@ var resetCookies = function () {
 
 $.fn.setWarning = function () {
     var args = arguments[0];
+
     $(this).hide();
     $(this).empty();
+
     $(this).html("<div>$$e</div>".render2({
         e: args.msg
     }));
-    $(this).show("slow");
+
+    if(args.msg){
+       $(this).show("slow");
+    }
 };
 $.fn.clearWarning = function () {
     $(this).hide();
@@ -550,6 +555,7 @@ var resetDashboard = function () {
     $("#top .top-right .logo").empty();
     $("#top .top-right .logo").addClass("empty");
     $("#user-info").remove();
+
     $("#menu ul.menu li").remove();
     $("#menu ul.menu").append("<li class='selected'>$$ee</li>".render({
         ee: 'Entrar'
@@ -1167,9 +1173,9 @@ var buildIndicatorHistory = function (args) {
                     $.each(headers, function (index2, value2) {
                         if (data.rows[index].valores[index2] && data.rows[index].valores[index2].value != "-" && data.rows[index].valores[index2].value != null && data.rows[index].valores[index2].value != undefined) {
 			    if ( isNaN(data.rows[index].valores[index2].value)){
-				
+
 				history_table += "<td class='valor' title='$$data' value-id='$$id' variable-id='$$variable_id'>$$valor <a href='javascript: void(0);' class='delete delete-item' title='$$title' alt='$$title'>$$e</a></td>".render2({
-				    valor: data.rows[index].valores[index2].value,					
+				    valor: data.rows[index].valores[index2].value,
 				    data: $.convertDate(data.rows[index].valores[index2].value_of_date, "T"),
 				    id: data.rows[index].valores[index2].id,
 				    variable_id: data.rows[index].valores[index2].variable_id,
@@ -1187,7 +1193,7 @@ var buildIndicatorHistory = function (args) {
 				    variable_id: data.rows[index].valores[index2].variable_id,
 				    title: "Apagar valor",
 								    e: "X"
-				
+
 				});
 			    }
 
